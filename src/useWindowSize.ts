77973eafcfa4d9e4363b1c3167d51e35646dacd4
Ekil <keithleaves@gmail.com>
2021-08-03 15:30:43 +0800

    init

---
+++ src/useWindowSize.ts
@@ -0,0 +1,16 @@
+import { useState, useLayoutEffect } from 'react'
+
+export function useWindowSize() {
+  const [size, setSize] = useState<[width: number, height: number]>([0, 0])
+
+  useLayoutEffect(() => {
+    function updateSize() {
+      setSize([window.innerWidth, window.innerHeight])
+    }
+    updateSize()
+    window.addEventListener('resize', updateSize)
+    return () => window.removeEventListener('resize', updateSize)
+  }, [])
+
+  return size
+}
